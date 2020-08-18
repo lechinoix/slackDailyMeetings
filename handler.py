3dacc6ef82ca58551de7c4f1d154d7213c80dda9
lechinoix <nicolas.ngomai@gmail.com>
2020-08-18 11:15:41 +0200

    Add daily mail message

--- handler.py
+++ handler.py
@@ -5,6 +5,19 @@ import os
 
 headers = {"Content-type": "application/json", "Authorization": "Bearer " + os.environ["SLACK_TOKEN"]}
 
+def send_message(message):
+    body = {
+        "channel": get_channel_id(),
+        "text": message,
+        "link_names": True
+    }
+    response = requests.post("https://slack.com/api/chat.postMessage", json=body, headers=headers)
+
+    return {
+        "status": response.status_code,
+        "reason": response.text
+    }
+
 def send_daily_message(event, context):
     daily_message = """@here Bonjour à tous, c'est bientôt l'heure du daily !
 
@@ -17,17 +30,16 @@ Pour rendre le point efficace, répondez à ces trois questions en réponse à c
 On se retrouve à 9h30 :)
     """
 
-    body = {
-        "channel": get_channel_id(),
-        "text": daily_message,
-        "link_names": True
-    }
-    response = requests.post("https://slack.com/api/chat.postMessage", json=body, headers=headers)
+    return send_message(daily_message)
 
-    return {
-        "status": response.status_code,
-        "reason": response.text
-    }
+def send_daily_mail_message(event, context):
+    daily_mail_message = """@here Et pour aider le rédacteur du daily mail, remplissez la google sheet suivante : https://docs.google.com/spreadsheets/d/1ffVvOTIw2fPID_4lRDyOa2v0q_TnuOkWZGU4F2LjHoQ/edit#gid=0
+
+Bonne journée !
+    """
+
+    return send_message(daily_mail_message)
+
 
 def get_channel_id():
     return next(
